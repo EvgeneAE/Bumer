e60ae3042ae8cdef53c3296256a24f97e4335cd0
Matvey <lorrik1703@yandex.ru>
2023-02-26 15:58:40 +0300

    test layer soundsFunk

---
+++ testingSaunds.js
@@ -0,0 +1,16 @@
+const player = require('play-sound')((opts = {}));
+
+//testing code
+//! Вставка кода в блок класса в Game.js, тостовый набросок
+//фон
+function musicPlayGame() {
+  this.audioGame = player.play(`${__dirname}/sounds/zvonokBumer.mp3`, function (err) {
+    if (err && !err.killed) throw err;
+  });
+}
+
+function musicPlayDied() {
+  player.play(`${__dirname}/sounds/dimoon.mp3`, function (err) {
+    if (err) throw err;
+  });
+}
