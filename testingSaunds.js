62882ddaf5d3fd4206912b9a4654790f5b4b0b98
Matvey <lorrik1703@yandex.ru>
2023-02-27 13:22:05 +0300

    release fin

--- testingSaunds.js
+++ testingSaunds.js
@@ -3,6 +3,7 @@ const player = require('play-sound')((opts = {}));
 //testing code
 //! Вставка кода в блок класса в Game.js, тостовый набросок
 //фон
+
 function musicPlayGame() {
   this.audioGame = player.play(`${__dirname}/sounds/zvonokBumer.mp3`, function (err) {
     if (err && !err.killed) throw err;
